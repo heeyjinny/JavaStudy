85983f66ca9a5c0ecb578cfe2566bd85a4973f86
heeyjinny <heeyjinny@gmail.com>
2022-12-25 21:29:51 +0900

    test

--- src/ch01/verify/Example.java
+++ src/ch01/verify/Example.java
@@ -4,6 +4,6 @@ public class Example {
 
 	public static void main(String[] args) {
 		System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
-		System.out.println("깃허브 커밋 테스트...");
+		System.out.println("깃허브 커밋 테스트...두 번째...");
 	}
 }
