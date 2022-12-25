c52b6c663a291c1442e8e1e12711a31df23e0060
heeyjinny <heeyjinny@gmail.com>
2022-12-25 21:20:02 +0900

    1

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,15 @@
+package ch01.sec11;
+/**
+ * @author heeyjinny
+ */
+/*
+장제목: 1장 자바 시작하기
+작성일: 2022.12.24
+ */
+public class Hello {
+	//프로그램 실행 진입
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행
+		System.out.println("Hello Java!!");
+	}
+}
