0d4df75bd70ed991cb02b21b48a1072219f201bb
heeyjinny <heeyjinny@gmail.com>
2023-01-06 16:50:57 +0900

    Variable

---
+++ src/ch02/sec01/VariableInitializationEx.java
@@ -0,0 +1,19 @@
+package ch02.sec01;
+
+public class VariableInitializationEx {
+
+	public static void main(String[] args) {
+		//변수 선언
+		int value;
+		//변수 선언 시 초기화를 꼭 해줘야 쓸 수 있음
+		value = 10;
+		
+		//연산 결과를 변수의 초기값으로 대입하기
+		int result = value + 10;
+		
+		//결과를 가지고 있는 변수 콘솔에 출력
+		System.out.println(result);
+		
+	}
+
+}
